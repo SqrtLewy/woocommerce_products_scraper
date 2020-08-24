ddf8137ba9abec73b5ef6f846594e0b77ca57da5
SqrtLewy <cramenpc@gmail.com>
2020-08-24 10:28:32 +0200

    save data as csv

--- scraper.js
+++ scraper.js
@@ -3,39 +3,41 @@ const Json2csv = require('json2csv').Parser;
 const puppeteer = require('puppeteer');
 
 (async () => {
-  try {
-    // open browser
-    var browser = await puppeteer.launch({ headless: true });
-    // open a new page
-    var page = await browser.newPage();
-    // enter url
-    await page.goto(`url`);
-	// a selector that needs to load
-    await page.waitForSelector("#content");
+    try {
+        // open browser
+        let browser = await puppeteer.launch({headless: true});
+        // open a new page
+        let page = await browser.newPage();
+        // enter url
+        await page.goto(`url`);
+        // a selector that needs to load
+        await page.waitForSelector("#content");
 
-    var urls = await page.evaluate(() => {
-	  // select a selector with links
-      var titleNodeList = document.querySelectorAll('li.product-grid-view > div > div > div > h3 > a');
-      var titleLinkArray = [];
-      for (var i = 0; i < titleNodeList.length; i++) {
-        titleLinkArray[i] = {
-		  // link selection
-          url: titleNodeList[i].getAttribute("href"),
-        };
-      }
-      return titleLinkArray;
-	});
-    await browser.close();
-    // Writing links inside a json file
-    fs.writeFile("urls.json", JSON.stringify(urls), function(err) {
-      if (err) throw err;
-      console.log("File saved!");
-    });
-    console.log("Browser Closed");
-  } catch (err) {
-    // Catch and display errors
-    console.log((err));
-    await browser.close();
-    console.log(error("Browser Closed"));
-  }
+        let urls = await page.evaluate(() => {
+            // select a selector with links
+            let titleNodeList = document.querySelectorAll('li.product-grid-view > div > div > div > h3 > a');
+            let titleLinkArray = [];
+            for (let i = 0; i < titleNodeList.length; i++) {
+                titleLinkArray[i] = {
+                    // link selection
+                    url: titleNodeList[i].getAttribute("href"),
+                };
+            }
+            return titleLinkArray;
+        });
+        await browser.close();
+        // Writing links inside a json file
+		const j2csv = new Json2csv;
+		const csv = j2csv.parse(urls);
+		fs.writeFileSync('./links.csv',csv,'utf-8', function (err) {
+            if (err) throw err;
+            console.log("File saved!");
+        });
+        console.log("Browser Closed");
+    } catch (err) {
+        // Catch and display errors
+        console.log((err));
+        await browser.close();
+        console.log(error("Browser Closed"));
+    }
 })();
--- scraper2.js
+++ scraper2.js
@@ -3,48 +3,50 @@ const Json2csv = require('json2csv').Parser;
 const puppeteer = require('puppeteer');
 
 (async () => {
-  try {
-    // open browser
-    var browser = await puppeteer.launch({ headless: true });
-    // open a new page
-    var page = await browser.newPage();
-    // enter url
-    await page.goto(`url`);
-	// a selector that needs to load
-    await page.waitForSelector("#content");
+    try {
+        // open browser
+        let browser = await puppeteer.launch({headless: true});
+        // open a new page
+        let page = await browser.newPage();
+        // enter url
+        await page.goto(`url`);
+        // a selector that needs to load
+        await page.waitForSelector("#content");
 
-      var products = await page.evaluate(() => {
-	  // select a selector with links to photos
-	  var getimages = document.querySelectorAll('div.attachment-shop_thumbnail > a > img[src*="/wp-content"]');
-	  // select only links
-	  var images = [].map.call(getimages, img => img.src);
-      var title = document.querySelector('h1.product_title').textContent;
-	  var description = document.querySelector('.post-content').textContent;
-	  var price = document.querySelector('p.price > span:nth-child(1)').textContent;
-	  var categories = document.querySelector('.posted_in').textContent;
-      var productsArray = [];
-      for (var i = 0; i < 160; i++) {
-        productsArray[i] = {
-		  images: images,
-          title: title,
-		  descrption: description,
-		  price: price,
-		  categories: categories
-        };
-      }
-      return productsArray;
-    });
-    await browser.close();
-    // Writing links inside a json file
-    fs.writeFile("products.json", JSON.stringify(products), function(err) {
-      if (err) throw err;
-      console.log("File saved!");
-    });
-    console.log("Browser Closed");
-  } catch (err) {
-    // Catch and display errors
-    console.log((err));
-    await browser.close();
-    console.log(error("Browser Closed"));
-  }
+        let products = await page.evaluate(() => {
+            // select a selector with links to photos
+            let getimages = document.querySelectorAll('div.attachment-shop_thumbnail > a > img[src*="/wp-content"]');
+            // select only links
+            let images = [].map.call(getimages, img => img.src);
+            let title = document.querySelector('h1.product_title').textContent;
+            let description = document.querySelector('.post-content').textContent;
+            let price = document.querySelector('p.price > span:nth-child(1)').textContent;
+            let categories = document.querySelector('.posted_in').textContent;
+            let productsArray = [];
+            for (let i = 0; i < 160; i++) {
+                productsArray[i] = {
+                    images: images,
+                    title: title,
+                    descrption: description,
+                    price: price,
+                    categories: categories
+                };
+            }
+            return productsArray;
+        });
+        await browser.close();
+        // Writing links inside a json file
+		const j2csv = new Json2csv;
+		const csv = j2csv.parse(products);
+		fs.writeFileSync('./products.csv',csv,'utf-8', function (err) {
+            if (err) throw err;
+            console.log("File saved!");
+        });
+        console.log("Browser Closed");
+    } catch (err) {
+        // Catch and display errors
+        console.log((err));
+        await browser.close();
+        console.log(error("Browser Closed"));
+    }
 })();
